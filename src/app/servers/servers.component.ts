47921c6b880e6f1c7e80cb3cc62c805e4dac1d6b
Mag <magali.dhyne@inforius.be>
2020-10-08 10:34:28 +0200

    directives

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector : 'app-servers',  //element <app-servers></app-servers>
+  //selector: '[app-servers]', attribut <div app-servers>
+  //selector: '.app-servers',  class but not id
+  templateUrl : './servers.component.html',
+  //template: `
+  //<app-server></app-server>
+  //<app-server></app-server>`,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'testServer';
+  serverCreated = false;
+
+  userName = '';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+   }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.serverCreated = true; //the p is not in the dom then appear with ngIf on true
+    //this.serverCreationStatus = "Server was created! Name is " + this.serverName;
+  }
+  //onUpdateServerName(event : Event){
+  //  this.serverName = (<HTMLInputElement>event.target).value;
+  //}
+  
+  onResetUserName(){
+
+    this.userName = '';
+  }
+  onUpdateUserName(event : Event){
+    this.userName = (<HTMLInputElement>event.target).value;
+  }
+
+}
